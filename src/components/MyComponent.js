bcbe5245f377c9dac2deb74acd77941929e24296
Nguyễn Hoàng Hải <ln26805@gmail.com>
2022-11-03 22:25:21 +0700

    upp

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,45 @@
 import React from "react";
 
 class MyCoponent extends React.Component {
+  state = {
+    name: "LoganZ",
+    age: 20,
+  };
+
+  handleClick = () => {
+    this.setState({
+      name: "HoangHai",
+      age: 21,
+    });
+  };
+
+  handleOnMouseOver = (event) => {
+    this.setState({
+      name: "HoangHai",
+      age: 20,
+    });
+  };
+
+  handleOnMouseOut = () => {
+    this.setState({
+      name: "LoganZ",
+      age: 21,
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1>My Component</h1>
-        {Math.random()}
+        <h1>
+          My name is {this.state.name} and I am {this.state.age}
+        </h1>
+        <button onClick={this.handleClick}>Click Me</button>
+        <button
+          onMouseOver={this.handleOnMouseOver}
+          onMouseOut={this.handleOnMouseOut}
+        >
+          Hover Me
+        </button>
       </div>
     );
   }
