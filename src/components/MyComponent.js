8ee50d74fcdac6f407fc7762d21d863925f1d51c
Nguyễn Hoàng Hải <ln26805@gmail.com>
2022-11-03 21:30:10 +0700

    first commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyCoponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My Component</h1>
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyCoponent;
