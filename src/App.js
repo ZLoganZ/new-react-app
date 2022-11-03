7aa9fd8b3dcbc3e8ce6ce92036bbea27f2793acb
Nguyễn Hoàng Hải <ln26805@gmail.com>
2022-11-03 22:43:45 +0700

    arrow function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import "./App.css";
 import MyCoponent from "./components/MyComponent";
 
-function App() {
+const App = () => {
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -25,6 +25,6 @@ function App() {
       <MyCoponent />
     </div>
   );
-}
+};
 
 export default App;
