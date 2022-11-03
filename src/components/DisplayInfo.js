189ff6addb704600ad580b010bd211c1463cca7a
Nguyễn Hoàng Hải <ln26805@gmail.com>
2022-11-04 00:42:33 +0700

    hahaa

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,19 +1,40 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShowed: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowed: !this.state.isShowed,
+    });
+  };
   render() {
     const { listUsers } = this.props;
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <h1>
-                My name is {user.name} and I am {user.age}
-              </h1>
-            </div>
-          );
-        })}
+        <div>
+          <button
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowed ? "Hide" : "Show"}
+          </button>
+        </div>
+        {this.state.isShowed && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={user.age > 20 ? "green" : "red"}>
+                  <h1>
+                    My name is {user.name} and I am {user.age}
+                  </h1>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
